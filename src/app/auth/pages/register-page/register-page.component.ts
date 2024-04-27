dc6ab70dd63fcfcc7a56e405fdc055f5f58185ad
bryanesteban <bryanesteban_123@hotmail.com>
2024-04-26 23:13:26 -0500

    rulers

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as customValidators from '../../../shared/validators/validators';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -9,8 +10,8 @@ export class RegisterPageComponent {
 
   public myForm : FormGroup = this.fb.group({
     name: ['', [ Validators.required]],
-    email: ['', [ Validators.required]],
-    username: ['', [ Validators.required]],
+    email: ['', [ Validators.required, Validators.pattern(customValidators.emailPattern)]],
+    username: ['', [ Validators.required, customValidators.cantBeStrider]],
     password: ['', [ Validators.required, Validators.minLength(6)]],
     password2: ['', [ Validators.required, Validators.minLength(6)]],
 
